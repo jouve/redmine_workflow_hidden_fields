82f1b9277df504e71d1d437adc594e1092ad6fd6
Cyril Jouve <cjouve@wyplay.com>
2018-03-26 12:58:51 +0200

    accelerate completely_hidden_attribute_names method

--- lib/redmine_workflow_hidden_fields/query_patch.rb
+++ lib/redmine_workflow_hidden_fields/query_patch.rb
@@ -16,11 +16,11 @@ module RedmineWorkflowHiddenFields
 
 
 		module InstanceMethods
-		
+
 			def available_and_visible_columns
 				available_columns.reject{|col| col.respond_to?(:custom_field) ? completely_hidden_fields.include?(col.custom_field.id.to_s) : completely_hidden_fields.include?(col.name)}
 			end
-			
+
 			# Returns an array of columns that can be used to group the results
 			def groupable_columns_with_hidden
 				available_and_visible_columns.select {|c| c.groupable}
@@ -40,45 +40,35 @@ module RedmineWorkflowHiddenFields
 
 			def available_block_columns_with_hidden
 				available_and_visible_columns.reject(&:inline?)
-			end  
-		
-		
-			def columns_with_hidden
-				columns_without_hidden.reject{|col| col.respond_to?(:custom_field) ? completely_hidden_fields.include?(col.custom_field.id.to_s) : completely_hidden_fields.include?(col.name)}				
 			end
-			
-			def available_filters_with_hidden	  
+
+
+			def columns_with_hidden
+				columns_without_hidden.reject{|col| col.respond_to?(:custom_field) ? completely_hidden_fields.include?(col.custom_field.id.to_s) : completely_hidden_fields.include?(col.name)}
+			end
+
+			def available_filters_with_hidden
 				unless @available_filters
 					available_filters_without_hidden
 					completely_hidden_fields.each {|field|
 						@available_filters.delete field
-					}					
+					}
 				end
-				@available_filters				
+				@available_filters
 			end
 
-			def completely_hidden_fields 
-				unless @completely_hidden_fields  
-					if project != nil 
-						@completely_hidden_fields = project.completely_hidden_attribute_names 
-					else 
-						@completely_hidden_fields = [] 
-						usr = User.current; 
-						first = true
-						all_projects.each { |prj|
-							if usr.roles_for_project(prj).count > 0 
-								if first 
-									@completely_hidden_fields = prj.completely_hidden_attribute_names(usr) 
-								else 
-									@completely_hidden_fields &= prj.completely_hidden_attribute_names(usr) 
-								end 
-								return @completely_hidden_fields if @completely_hidden_fields.empty? 
-								first = false 								
-							end 
-						} 
+			def completely_hidden_fields
+				unless @completely_hidden_fields
+					if project.nil?
+						projects = all_projects
+					else
+						projects = [project]
 					end
-				end				
-				return @completely_hidden_fields 
+					@completely_hidden_fields = projects.map { |prj|
+						prj.completely_hidden_attribute_names(User.current)
+					}.reject(&:empty?).reduce(&:&)
+				end
+				@completely_hidden_fields
 			end
 
 			def add_custom_field_filter_with_hidden(field, assoc=nil)
